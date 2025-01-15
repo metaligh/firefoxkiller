66d739bc3787128a4caa88b6d5f9a98be1a05d75
metaligh <metaligh@proton.me>
2025-01-15 12:10:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,13 @@ from datetime import datetime
 PBT_POWERSETTINGCHANGE = 0x8013
 
 
-# Структура для хранения данных, передаваемых через lparam
 class POWERBROADCAST_SETTING(Structure):
-    _fields_ = [("PowerSetting", c_void_p),  # Указатель на GUID настройки
-                ("DataLength", c_uint),  # Длина данных
-                ("Data", c_void_p)]  # Указатель на данные (состояние устройства)
+    _fields_ = [("PowerSetting", c_void_p),  
+                ("DataLength", c_uint), 
+                ("Data", c_void_p)]  
 
 
 def log_info(msg):
-    """Логирование в файл с датой и временем события"""
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log_message = f"[{timestamp}] {msg}"
     print(log_message)
@@ -30,7 +28,6 @@ def log_info(msg):
 
 
 def terminate_firefox_processes():
-    """Завершаем все процессы firefox.exe"""
     for proc in psutil.process_iter(['pid', 'name']):
         if proc.info['name'].lower() == 'firefox.exe':
             try:
@@ -41,35 +38,28 @@ def terminate_firefox_processes():
 
 
 def process_power_setting_change(lparam):
-    """Обработка изменения настроек питания"""
-    # Попробуем использовать ctypes.cast для приведения lparam к указателю на структуру
     setting = ctypes.cast(lparam, POINTER(POWERBROADCAST_SETTING))
 
-    # Разбираем данные в lparam
     power_setting_guid = setting.contents.PowerSetting
     data_length = setting.contents.DataLength
     data = setting.contents.Data
 
-    # Преобразуем GUID в строковый вид (если это GUID, а не числовое значение)
     power_setting_guid_str = str(uuid.UUID(int=power_setting_guid))
 
-    # Логируем GUID
     log_info(f"Power Setting GUID: {power_setting_guid_str}")
 
-    # Если данные равны 4, экран погас
     if data_length > 0:
         if data == 4:
             log_info("Screen turned off. Terminating firefox processes.")
-            terminate_firefox_processes()  # Закрыть все процессы Firefox
+            terminate_firefox_processes()  
 
 
 def wndproc(hwnd, msg, wparam, lparam):
-    """Обработка сообщений"""
     if msg == win32con.WM_POWERBROADCAST:
         if wparam == PBT_POWERSETTINGCHANGE:
             log_info('Power setting changed...')
             try:
-                process_power_setting_change(lparam)  # Обработка данных из lparam
+                process_power_setting_change(lparam)  
             except Exception as e:
                 log_info(f"Error processing power setting change: {e}")
                 return 0
@@ -78,10 +68,8 @@ def wndproc(hwnd, msg, wparam, lparam):
 
 
 def main():
-    """Главная функция для запуска обработчика сообщений"""
     log_info("*** STARTING ***")
 
-    # Инициализация окна
     hinst = win32api.GetModuleHandle(None)
     wndclass = win32gui.WNDCLASS()
     wndclass.hInstance = hinst
@@ -111,7 +99,6 @@ def main():
     else:
         log_info(f"Window created successfully with hwnd: {hwnd}")
 
-    # Регистрация уведомлений для изменений настроек питания (монитор, система и т.д.)
     register_function = ctypes.windll.user32.RegisterPowerSettingNotification
 
     guids_info = {
@@ -119,13 +106,12 @@ def main():
         'GUID_SYSTEM_AWAYMODE': '{98a7f580-01f7-48aa-9c0f-44352c29e5C0}',
     }
 
-    hwnd_pointer = ctypes.cast(hwnd, ctypes.c_void_p)  # Используем cast для приведения типа hwnd
+    hwnd_pointer = ctypes.cast(hwnd, ctypes.c_void_p)  
     for name, guid_info in guids_info.items():
         result = register_function(hwnd_pointer, GUID(guid_info), 0)
         log_info(f"Registering {name} - result: {result}")
 
     log_info("\nEntering loop...")
-    # Основной цикл для обработки сообщений
     while True:
         win32gui.PumpWaitingMessages()
         time.sleep(1)
